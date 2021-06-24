b3a6d1a22f75faab21ade062c073131a246e59a4
Lucky Susanto <luckyblock0@gmail.com>
2021-06-24 14:17:23 +0700

    CRUD completed

--- main.go
+++ main.go
@@ -16,6 +16,11 @@ type News struct {
 	Title       string   `json:"Title"`
 	Description string   `json:"Description"`
 	Tag         []string `json:"Tag"`
+	Status      string   `json:"Status"`
+}
+
+type Error struct {
+	Error string `json:"Error"`
 }
 
 type allNews []News
@@ -27,21 +32,23 @@ var news = allNews{
 		Title:       "Silver Price Skyrocks After Reddit Attack",
 		Description: "Lorum Ipsum Potum",
 		Tag:         []string{"High Frequency", "Risky Trade"},
+		Status:      "Publish",
 	},
 	{
 		ID:          "2",
 		Title:       "Doge Coin Predicted To Skyrock Again Next Year",
 		Description: "Lorum Ipsum Potum",
 		Tag:         []string{"Long Trade", "Risky Trade"},
+		Status:      "Draft",
 	},
 }
 
 /*Service*/
 func homeLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome home!")
+	fmt.Fprintf(w, "Home Page!")
 }
 
-func createEvent(w http.ResponseWriter, r *http.Request) {
+func createNews(w http.ResponseWriter, r *http.Request) {
 	var newNews News
 	// Convert r.Body into a readable formart
 	reqBody, err := ioutil.ReadAll(r.Body)
@@ -50,7 +57,7 @@ func createEvent(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.Unmarshal(reqBody, &newNews)
-
+	newNews.Status = "Publish"
 	// Add the newly created event to the array of events
 	news = append(news, newNews)
 
@@ -60,7 +67,7 @@ func createEvent(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(newNews)
 }
 
-func getOneEvent(w http.ResponseWriter, r *http.Request) {
+func getOneNews(w http.ResponseWriter, r *http.Request) {
 	// Get the ID from the url
 	newsID := mux.Vars(r)["id"]
 
@@ -68,47 +75,70 @@ func getOneEvent(w http.ResponseWriter, r *http.Request) {
 	// Use the blank identifier to avoid creating a value that will not be used
 	for _, singleNews := range news {
 		if singleNews.ID == newsID {
-			json.NewEncoder(w).Encode(singleNews)
+			if singleNews.Status == "Publish" {
+				json.NewEncoder(w).Encode(singleNews)
+			} else {
+				w.WriteHeader(http.StatusNoContent)
+				var customError Error
+				customError.Error = "The News you are searching is either not published or doesn't exist"
+				json.NewEncoder(w).Encode(customError)
+			}
 		}
 	}
 }
 
-func getAllEvents(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(news)
+func getAllNews(w http.ResponseWriter, r *http.Request) {
+	var publishedNews = allNews{}
+
+	for _, singleNews := range news {
+		if singleNews.Status == "Publish" {
+			publishedNews = append(publishedNews, singleNews)
+		}
+	}
+	json.NewEncoder(w).Encode(publishedNews)
 }
 
-func updateEvent(w http.ResponseWriter, r *http.Request) {
+func updateNews(w http.ResponseWriter, r *http.Request) {
 	// Get the ID from the url
 	newsID := mux.Vars(r)["id"]
 	var updatedNews News
 	// Convert r.Body into a readable formart
 	reqBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		fmt.Fprintf(w, "Kindly enter data with the News title and description only in order to update")
+		fmt.Fprintf(w, "Kindly enter data with the News title or description or tags or status in order to update")
 	}
 
 	json.Unmarshal(reqBody, &updatedNews)
 
 	for i, singleNews := range news {
 		if singleNews.ID == newsID {
-			singleNews.Title = updatedNews.Title
-			singleNews.Description = updatedNews.Description
+			if updatedNews.Title != "" {
+				singleNews.Title = updatedNews.Title
+			}
+			if updatedNews.Description != "" {
+				singleNews.Description = updatedNews.Description
+			}
+			if updatedNews.Tag != nil {
+				singleNews.Tag = updatedNews.Tag
+			}
+			if updatedNews.Status != "" {
+				singleNews.Status = updatedNews.Status
+			}
 			news[i] = singleNews
 			json.NewEncoder(w).Encode(singleNews)
 		}
 	}
 }
 
-func deleteEvent(w http.ResponseWriter, r *http.Request) {
+func deleteNews(w http.ResponseWriter, r *http.Request) {
 	// Get the ID from the url
 	eventID := mux.Vars(r)["id"]
 
 	// Get the details from an existing event
-	// Use the blank identifier to avoid creating a value that will not be used
 	for i, singleNews := range news {
 		if singleNews.ID == eventID {
-			news = append(news[:i], news[i+1:]...)
-			fmt.Fprintf(w, "The event with ID %v has been deleted successfully", eventID)
+			news[i].Status = "Deleted"
+			fmt.Fprintf(w, "The event with ID %v has been marked for deletion", eventID)
 		}
 	}
 }
@@ -117,10 +147,10 @@ func deleteEvent(w http.ResponseWriter, r *http.Request) {
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
-	router.HandleFunc("/news", createEvent).Methods("POST")
-	router.HandleFunc("/news/all", getAllEvents).Methods("GET")
-	router.HandleFunc("/news/{id}", getOneEvent).Methods("GET")
-	router.HandleFunc("/news/{id}", updateEvent).Methods("PATCH")
-	router.HandleFunc("/news/{id}", deleteEvent).Methods("DELETE")
+	router.HandleFunc("/news", createNews).Methods("POST")
+	router.HandleFunc("/news/all", getAllNews).Methods("GET")
+	router.HandleFunc("/news/{id}", getOneNews).Methods("GET")
+	router.HandleFunc("/news/{id}", updateNews).Methods("PATCH")
+	router.HandleFunc("/news/{id}", deleteNews).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
