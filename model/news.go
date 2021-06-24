e9b098a42e9825a4e8c6a0e51c2e274e46e71094
Lucky Susanto <luckyblock0@gmail.com>
2021-06-24 19:26:01 +0700

    Repush models

--- model/news.go
+++ model/news.go
@@ -1,13 +1,9 @@
 package model
 
 type News struct {
-	NID        string `json:"NID"`
-	Title      string `json:"Title"`
-	Desciption string `json:"Description"`
-	Tag        []Tags `json:"Tags"`
-}
-
-type Tags struct {
-	TID string `json:"TID"`
-	Tag string `json:"Tag"`
+	ID          string   `json:"ID"`
+	Title       string   `json:"Title"`
+	Description string   `json:"Description"`
+	Tag         []string `json:"Tag"`
+	Status      string   `json:"Status"`
 }
